7c0fc44693e9c238b7fd9fd36729268ed75a9980
timmySpark <timmyspark1@gmail.com>
2023-08-20 08:43:44 -0700

    [feat] : added new testcases

--- test/task0_test.c
+++ test/task0_test.c
@@ -16,18 +16,39 @@ int main(void)
 	len1 = _printf("Hello %s!\n", "World");
 	len2 =  printf("Hello %s!\n", "World");
 	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
+	len1 = _printf("%s\n", "");
+	len2 =  printf("%s\n", "");
+	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
+	len1 = _printf("%s\n", "123 ABC \t ^%");
+	len2 =  printf("%s\n", "123 ABC \t ^%");
+	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
+	len1 = _printf("%s", NULL);
+	printf("_printf length: %d\n", len1);
+	_printf(NULL);
 
 	/* Print a Character */
-
-	len1 = _printf("Character: %c\n", 'A');
-	len2 =  printf("Character: %c\n", 'A');
+	len1 = _printf("Character: %c\n", 7);
+	len2 =  printf("Character: %c\n", 7);
 	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
 
 	/* Print Percentage (%) */
 
-	len1 = _printf("Percentage: %%\n");
-	len2 =  printf("Percentage: %%\n");
+	len1 = _printf("Percentage: [%%]\n");
+	len2 =  printf("Percentage: [%%]\n");
 	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
+
+	/* Print Unknown Specifier *
+	len1 = _printf("Unknown:[%z]\n", 67);
+	len2 =  printf("Unknown:[%z]\n", 67);
+	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);*/
+
+	/* Print Multiple Specifiers */
+	len1 = _printf("Character: %c, String: %s\n", 'A', "Hello");
+	len2 =  printf("Character: %c, String: %s\n", 'A', "Hello");
+	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
+	
 
 	return (0);
 }
