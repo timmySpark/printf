34279686e4ba91abb5ff23c3792c83aeba0a1b99
timmySpark <timmyspark1@gmail.com>
2023-08-19 17:59:09 -0700

    [feat] :- wrote a recreation of _printf function that checks for specific patterns at the moment

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - Print formatted strings
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: Number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int char_count = 0;
+	int (*func)(va_list);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			func = get_spec(*format);
+			char_count += func(args);
+		}
+		else
+		{
+			_putchar(*format);
+			char_count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (char_count);
+}
