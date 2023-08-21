0e5e17358ba5b4b6ba9909a4089c70ab6ffc9fe7
timmySpark <timmyspark1@gmail.com>
2023-08-20 17:54:26 -0700

    [feat] :- rewrote the binary function using mask

--- my_func_num.c
+++ my_func_num.c
@@ -15,11 +15,6 @@ int convert_binary(va_list args)
 	num = va_arg(args, unsigned int);
 	mask = 1 << (sizeof(unsigned int) * 8 - 1);
 
-	if (!num)
-	{
-		num = 0;
-		return (1);
-	}
 	if (num == 0)
 	{
 		_putchar('0');
@@ -32,11 +27,17 @@ int convert_binary(va_list args)
 	while (mask)
 	{
 		if (num & mask)
+		{
 			_putchar('1');
+			count++;
+		}
 		else
+		{
 			_putchar('0');
+			count++;
+		}
 
-		count++;
+		/*count++*/;
 		mask >>= 1;
 	}
 
