7d83854dd4d527d0544acbea3fb42cbc1119da85
timmySpark <timmyspark1@gmail.com>
2023-08-21 19:07:08 -0700

    [feat] :- added new function that prints addresses

--- my_func_num.c
+++ my_func_num.c
@@ -43,3 +43,67 @@ int convert_binary(va_list args)
 
 	return (count);
 }
+
+/**
+ * convert_hex - print an unsigned long int as a hexadecimal number
+ * @n: Number to be printed
+ *
+ * Return: Number of digits printed
+ */
+
+int convert_hex(unsigned long int n)
+{
+	int count = 0;
+
+	if (n >= 16)
+		count += convert_hex(n / 16);
+
+	if (n % 16 < 10)
+	{
+		buffer_store_char((n % 16) + '0');
+		count++;
+	}
+	else
+	{
+		buffer_store_char((n % 16) - 10 + 'a');
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_pointer - print a memory address
+ * @args: arguments to be inputted
+ *
+ * Return: Number of characters printed;
+ */
+
+int print_pointer(va_list args)
+{
+	void *num = va_arg(args, void *);
+	unsigned long int address = (unsigned long int) num;
+	int count = 0;
+	char *null_str;
+
+	if (num == NULL)
+	{
+		null_str = "(nil)";
+
+		while (*null_str)
+		{
+			buffer_store_char(*null_str++);
+			count++;
+		}
+
+		return (count);
+	}
+	else
+	{
+		buffer_store_char('0');
+		buffer_store_char('x');
+		count += 2;
+		count += convert_hex(address);
+		return (count);
+	}
+}
