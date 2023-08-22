a7e5555143997c3eda4f3689bf722aa39f0d2c67
timmySpark <timmyspark1@gmail.com>
2023-08-22 10:12:05 -0700

    [feat] :- added new testcase to check %r conversion specifier

--- test/main.c
+++ test/main.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int len1, len2;
+	char test[] = "gnirts a si sihT";
 	void *addr;
 	addr = (void *)0x7ffe637541f0;
 
@@ -24,12 +25,11 @@ int main(void)
 	len1 = _printf("%s\n", "123 ABC \t ^%");
 	len2 =  printf("%s\n", "123 ABC \t ^%");
 	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
-	len1 = _printf("%s\n", NULL);
-	printf("_printf length: %d\n", len1);
-	len2 = _printf(NULL);
-	printf("NULL len: %d\n", len2);
+	len1 = _printf("Reversed string: %r\n", test);
+	printf("_printf Length: %d\n", len1);
 
 	/* Print a Character */
+
 	len1 = _printf("Character: %c\n", 7);
 	len2 =  printf("Character: %c\n", 7);
 	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
@@ -43,24 +43,40 @@ int main(void)
 	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
 
 
-	/* Print Unknown Specifier *
-	len1 = _printf("Unknown:[%z]\n", 67);
-	len2 =  printf("Unknown:[%z]\n", 67);
-	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);*/
+	/* Print Unknown Specifier */
+
+   	len1 = _printf("Unknown:[%y]\n");
+	printf("_printf Length: %d\n", len1);
 
 	/* Print Multiple Specifiers */
+
 	len1 = _printf("Character: %c, String: %s\n", 'A', "Hello");
 	len2 =  printf("Character: %c, String: %s\n", 'A', "Hello");
 	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
 
 	/* Print Binary Specifier */
+
 	len1 = _printf("Binary: %b\n", 98);
 	printf(" _print Length: %d\n", len1);
 
 	/* Print Address */
+
 	len1 =_printf("Address:[%p]\n", addr);
 	len2 = printf("Address:[%p]\n", addr);
 	printf("Printf Length: %d | _printf Length: %d\n", len2, len1);
+
+	/** Edge Cases (as many as i can remember) **/
+
+	len1 = _printf("%");
+	printf("_printf Length: %d\n", len1);
+	len1 = _printf("% ");
+	printf("_printf Length: %d\n", len1);
+	len2 = _printf("%\0");
+	printf("_printf Length: %d\n", len2);
+	len1 = _printf("%s\n", NULL);
+	printf("_printf length: %d\n", len1);
+	len2 = _printf(NULL);
+	printf("null_len: %d\n", len2);
 
 	return (0);
 }
