08763e05dcd692841cc5aa83220f1e26f3d8f41c
timmySpark <timmyspark1@gmail.com>
2023-08-22 17:22:41 -0700

    [feat]:- trying to work on a better solution for the code joseph wrote

---
+++ print_integers.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _put_int - print integer numbers
+ * @num: integer to be printed
+ * @count: count
+ *
+ * Return: numbers of integers counted
+ */
+
+int _put_int(int num, int count)
+{
+	int n = num;
+
+	if (n == 0)
+	{
+		buffer_store_char('0');
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		buffer_store_char('-');
+		count++;
+		n = -n;
+	}
+
+	if (n / 10)
+		count = _put_int(n / 10, count);
+
+	buffer_store_char(n % 10 + '0');
+	count++;
+	return (count);
+
+}
+
+
+/**
+ * print_int - print integer numbers
+ * @arg: Arguments List
+ *
+ * Return: numbers of integers counted
+ */
+
+int print_int(va_list args)
+{
+	int value = va_arg(args, int);
+	int count = 0;
+
+	count = _put_int(value, 0);
+
+	return (count);
+}
+
